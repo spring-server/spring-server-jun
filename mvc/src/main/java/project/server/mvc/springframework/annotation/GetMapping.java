b10c824dcad0d877233ab06285336a7769617a45
devjun10 <devjun1023@gmail.com>
2024-01-23 02:18:45 +0900

    feat: Annotation 추가 - 스프링 컨테이너를 구현하기 위한 어노테이션 추가

---
+++ mvc/src/main/java/project/server/mvc/springframework/annotation/GetMapping.java
@@ -0,0 +1,36 @@
+package project.server.mvc.springframework.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import project.server.mvc.servlet.http.HttpMethod;
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@RequestMapping(method = HttpMethod.GET)
+public @interface GetMapping {
+
+    @AliasFor(annotation = RequestMapping.class)
+    String name() default "";
+
+    @AliasFor(annotation = RequestMapping.class)
+    String[] value() default {};
+
+    @AliasFor(annotation = RequestMapping.class)
+    String[] path() default {};
+
+    @AliasFor(annotation = RequestMapping.class)
+    String[] params() default {};
+
+    @AliasFor(annotation = RequestMapping.class)
+    String[] headers() default {};
+
+    @AliasFor(annotation = RequestMapping.class)
+    String[] consumes() default {};
+
+    @AliasFor(annotation = RequestMapping.class)
+    String[] produces() default {};
+}
