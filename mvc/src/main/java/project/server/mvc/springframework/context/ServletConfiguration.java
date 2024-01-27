a6a9678eddaa7b61802aa304f49625110bab35cc
devjun10 <devjun1023@gmail.com>
2024-01-27 17:02:55 +0900

    feature: 스프링 컨테이너 구현 - ComponentScan, 빈 등록 등 스프링 컨테이너 구현 - Reflections 라이브러리 사용

---
+++ mvc/src/main/java/project/server/mvc/springframework/context/ServletConfiguration.java
@@ -0,0 +1,14 @@
+package project.server.mvc.springframework.context;
+
+import project.server.mvc.springframework.annotation.Bean;
+import project.server.mvc.springframework.annotation.Configuration;
+import project.server.mvc.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
+public class ServletConfiguration {
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+}
