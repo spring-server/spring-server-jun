cc79159d6ec23eb2348f4164b49f87e44080068c
devjun10 <devjun1023@gmail.com>
2024-01-27 10:11:24 +0900

    test: @BeforeEach 메서드 추가 - 테스트 멱등성을 위해 @BeforeEach 메서드 추가

--- app/src/test/java/project/server/app/test/integrationtest/IntegrationTestBase.java
+++ app/src/test/java/project/server/app/test/integrationtest/IntegrationTestBase.java
@@ -1,19 +1,28 @@
 package project.server.app.test.integrationtest;
 
+import org.junit.jupiter.api.BeforeEach;
+import project.server.app.core.domain.user.UserRepository;
+import project.server.app.core.web.user.persistence.UserPersistenceRepository;
 import project.server.mvc.springframework.context.ApplicationContext;
+import static project.server.mvc.springframework.context.ApplicationContext.getBean;
 import project.server.mvc.springframework.context.ApplicationContextProvider;
 
 public abstract class IntegrationTestBase {
 
-    protected ApplicationContext applicationContext;
+    protected final UserRepository userRepository;
 
     protected IntegrationTestBase() {
         try {
-            applicationContext = new ApplicationContext("project");
             ApplicationContextProvider provider = new ApplicationContextProvider();
-            provider.setApplicationContext(applicationContext);
+            provider.setApplicationContext(new ApplicationContext("project"));
+            this.userRepository = getBean(UserPersistenceRepository.class);
         } catch (Exception exception) {
             throw new RuntimeException(exception);
         }
+    }
+
+    @BeforeEach
+    void setUp() {
+        userRepository.clear();
     }
 }
