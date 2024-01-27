9a5f8d031ab1987b7d98679495b0e4944045bf58
devjun10 <devjun1023@gmail.com>
2024-01-27 19:08:24 +0900

    test: 사용자 이름 정규식 단위 테스트 추가 - 7~13자리 - 특수문자허용 X, 공백 포함 X

--- app/src/test/java/project/server/app/test/unittest/user/UsernameUnitTest.java
+++ app/src/test/java/project/server/app/test/unittest/user/UsernameUnitTest.java
@@ -1,7 +1,10 @@
 package project.server.app.test.unittest.user;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import project.server.app.core.domain.user.Username;
@@ -25,5 +28,20 @@ class UsernameUnitTest {
         Username differentUsername = new Username("Jobs-Steve");
 
         assertNotEquals(username, differentUsername);
+    }
+
+    @Test
+    @DisplayName("정의한 정규식을 통과해야 사용자 이름 객체가 생성된다.")
+    void usernameRegexTest() {
+        assertAll(
+            () -> assertDoesNotThrow(() -> new Username("ValidUsername")),
+            () -> assertDoesNotThrow(() -> new Username("Jun1234")),
+            () -> assertDoesNotThrow(() -> new Username("User_Jun-123")),
+            () -> assertThrows(IllegalArgumentException.class, () -> new Username("a")),
+            () -> assertThrows(IllegalArgumentException.class, () -> new Username("Invalid!@#")),
+            () -> assertThrows(IllegalArgumentException.class, () -> new Username("ThisIsWayTooLongUsername")),
+            () -> assertThrows(IllegalArgumentException.class, () -> new Username("")),
+            () -> assertThrows(IllegalArgumentException.class, () -> new Username("Invalid Name"))
+        );
     }
 }
