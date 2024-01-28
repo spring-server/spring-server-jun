c111de5bb9220cc01a56aaab709447fb90da03d8
devjun10 <devjun1023@gmail.com>
2024-01-28 21:23:50 +0900

    refactor: Application 구동 방식 리팩토링 - Application 생성자 조합으로 구동방식 변경

--- app/src/main/java/project/server/app/BlogApplication.java
+++ app/src/main/java/project/server/app/BlogApplication.java
@@ -1,10 +1,13 @@
 package project.server.app;
 
-import project.server.mvc.springframework.context.Application;
+import project.server.mvc.Application;
+import static project.server.mvc.tomcat.PortFinder.findPort;
 
 public class BlogApplication {
 
-    public static void main(String[] args) throws Exception {
-        Application.run(args);
+    public static void main(String[] args) {
+        int port = findPort(args);
+        Application application = new Application("project", port, 200);
+        application.start();
     }
 }
