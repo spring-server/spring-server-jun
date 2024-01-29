47425a5eb7e79bc44f83880a5e7635e4b6e3c1c6
devjun10 <devjun1023@gmail.com>
2024-01-29 20:42:31 +0900

    test: UserValidator 단위 테스트 작성

--- app/src/test/java/project/server/app/test/unittest/user/UserValidatorUnitTest.java
+++ app/src/test/java/project/server/app/test/unittest/user/UserValidatorUnitTest.java
@@ -1,11 +1,14 @@
 package project.server.app.test.unittest.user;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import project.server.app.common.exception.InvalidParameterException;
+import project.server.app.common.exception.UnAuthorizedException;
 import project.server.app.core.web.user.presentation.validator.UserValidator;
 
 @DisplayName("[UnitTest] 사용자 검증 단위 테스트")
@@ -34,5 +37,19 @@ class UserValidatorUnitTest {
         assertThatThrownBy(() -> validator.validateSignUpInfo("Steve-Jobs", parameter))
             .isInstanceOf(RuntimeException.class)
             .isExactlyInstanceOf(InvalidParameterException.class);
+    }
+
+    @Test
+    @DisplayName("사용자 아이디가 null이면 UnAuthorizedException이 발생한다.")
+    void sessionIdNullTest() {
+        assertThatThrownBy(() -> validator.validateSessionId(null))
+            .isInstanceOf(RuntimeException.class)
+            .isExactlyInstanceOf(UnAuthorizedException.class);
+    }
+
+    @Test
+    @DisplayName("사용자 아이디가 null이 아니라면 에러가 발생하지 않는다.")
+    void sessionIdValidateTest() {
+        assertDoesNotThrow(() -> validator.validateSessionId(1L));
     }
 }
