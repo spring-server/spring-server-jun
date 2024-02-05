43da321076a726fe1d369c9a228110c2a5cef141
devjun10 <devjun1023@gmail.com>
2024-02-06 01:41:34 +0900

    refactor: Repository 구현체 교체 - MySQL을 사용해 데이터를 조회하도록 구현체 교체

--- app/src/main/java/project/server/app/core/web/user/persistence/UserPersistenceRepository.java
+++ app/src/main/java/project/server/app/core/web/user/persistence/UserPersistenceRepository.java
@@ -1,90 +1,64 @@
 package project.server.app.core.web.user.persistence;
 
-import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.function.Predicate;
+import lombok.SneakyThrows;
+import project.server.app.common.configuration.DatabaseConfiguration;
 import project.server.app.core.domain.user.User;
 import project.server.app.core.domain.user.UserRepository;
-import project.server.app.core.web.user.exception.AlreadyRegisteredUserException;
+import project.server.jdbc.core.jdbc.JdbcTemplate;
 import project.server.mvc.springframework.annotation.Repository;
 
 @Repository
 public class UserPersistenceRepository implements UserRepository {
 
-    private static final Boolean ALREADY_EXIST = TRUE;
-    private static final Boolean NOT_FOUND = FALSE;
-
     private static final Map<Long, User> factory = new ConcurrentHashMap<>();
-    private static final Lock lock = new ReentrantLock();
-    private static final AtomicLong idGenerator = new AtomicLong(0L);
+
+    private final JdbcTemplate<User> jdbcTemplate;
+
+    private UserPersistenceRepository(DatabaseConfiguration dbConfig) throws IOException {
+        this.jdbcTemplate = dbConfig.jdbcTemplate();
+    }
 
     @Override
+    @SneakyThrows
     public User save(User user) {
-        lock.lock();
-        try {
-            Long id = idGenerator.incrementAndGet();
-            if (!user.isNew()) {
-                throw new AlreadyRegisteredUserException();
-            }
-            user.registerId(id);
-            factory.put(id, user);
-        } finally {
-            lock.unlock();
-        }
-        return user;
+        return jdbcTemplate.save(user);
     }
 
     @Override
+    @SneakyThrows
     public Optional<User> findById(Long userId) {
-        return Optional.ofNullable(factory.get(userId));
+        User findUser = jdbcTemplate.findById(User.class, userId);
+        return Optional.ofNullable(findUser);
     }
 
     @Override
+    @SneakyThrows
     public boolean existByName(String username) {
-        User findUser = factory.values().stream()
-            .filter(equalsUsername(username))
-            .findAny()
-            .orElseGet(() -> null);
-        return findUser != null ? ALREADY_EXIST : NOT_FOUND;
+        return jdbcTemplate.existsByField(User.class, "username", username);
     }
 
     @Override
-    public List<User> findAll() {
-        return factory.values().stream()
-            .toList();
-    }
-
-    @Override
+    @SneakyThrows
     public Optional<User> findByUsernameAndPassword(
         String username,
         String password
     ) {
-        return Optional.ofNullable(
-            factory.values().stream()
-                .filter(equalsUsername(username))
-                .filter(equalsPassword(password))
-                .findAny()
-                .orElseGet(() -> null)
-        );
+        return jdbcTemplate.findByUsernameAndPassword(User.class, username, password);
+    }
+
+    @Override
+    @SneakyThrows
+    public List<User> findAll() {
+        return jdbcTemplate.findAll(User.class);
     }
 
     @Override
     public void clear() {
         factory.clear();
-    }
-
-    private Predicate<User> equalsUsername(String username) {
-        return user -> user.getUsername().equals(username);
-    }
-
-    private Predicate<User> equalsPassword(String password) {
-        return user -> user.getPassword().equals(password);
     }
 }
