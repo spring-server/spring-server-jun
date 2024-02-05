9b33c1e912516ff2f6c9cb2873ed6d365edd37c7
devjun10 <devjun1023@gmail.com>
2024-02-06 01:39:54 +0900

    feat: 서비스 프록시 클래스 추가 - 핸들러에서 프록시를 호출하도록 프록시 클래스 추가

---
+++ app/src/main/java/project/server/app/core/web/user/application/service/UserLoginServiceProxy.java
@@ -0,0 +1,62 @@
+package project.server.app.core.web.user.application.service;
+
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import project.server.app.common.configuration.DatabaseConfiguration;
+import project.server.app.common.login.Session;
+import project.server.app.core.web.user.application.UserLoginUseCase;
+import static project.server.jdbc.core.transaction.DefaultTransactionDefinition.createTransactionDefinition;
+import project.server.jdbc.core.transaction.PlatformTransactionManager;
+import project.server.jdbc.core.transaction.TransactionStatus;
+import project.server.mvc.springframework.annotation.Component;
+
+@Slf4j
+@Component
+public class UserLoginServiceProxy implements UserLoginUseCase {
+
+    private final PlatformTransactionManager txManager;
+    private final UserLoginService target;
+
+    public UserLoginServiceProxy(
+        DatabaseConfiguration dbConfiguration,
+        UserLoginService target
+    ) throws IOException {
+        this.txManager = dbConfiguration.transactionManager();
+        this.target = target;
+    }
+
+    @Override
+    public Session login(
+        String username,
+        String password
+    ) {
+        TransactionStatus txStatus = getTransactionStatus(false);
+        log.debug("txStatus:{}", txStatus);
+        try {
+            Session findSession = target.login(username, password);
+            txManager.commit(txStatus);
+            return findSession;
+        } catch (Exception exception) {
+            txManager.rollback(txStatus);
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public Session findSessionById(Long userId) {
+        try {
+            return target.findSessionById(userId);
+        } catch (Exception exception) {
+            throw new RuntimeException();
+        }
+    }
+
+    private TransactionStatus getTransactionStatus(boolean readOnly) {
+        try {
+            return txManager.getTransaction(createTransactionDefinition(readOnly));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+}
---
+++ app/src/main/java/project/server/app/core/web/user/application/service/UserServiceProxy.java
@@ -0,0 +1,77 @@
+package project.server.app.core.web.user.application.service;
+
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import project.server.app.common.configuration.DatabaseConfiguration;
+import project.server.app.common.login.LoginUser;
+import project.server.app.core.domain.user.User;
+import project.server.app.core.web.user.application.UserDeleteUseCase;
+import project.server.app.core.web.user.application.UserSaveUseCase;
+import project.server.app.core.web.user.application.UserSearchUseCase;
+import static project.server.jdbc.core.transaction.DefaultTransactionDefinition.createTransactionDefinition;
+import project.server.jdbc.core.transaction.PlatformTransactionManager;
+import project.server.jdbc.core.transaction.TransactionStatus;
+import project.server.mvc.springframework.annotation.Component;
+
+@Slf4j
+@Component
+public class UserServiceProxy implements UserSaveUseCase, UserSearchUseCase, UserDeleteUseCase {
+
+    private final PlatformTransactionManager txManager;
+    private final UserService target;
+
+    public UserServiceProxy(
+        DatabaseConfiguration dbConfiguration,
+        UserService target
+    ) throws IOException {
+        this.txManager = dbConfiguration.transactionManager();
+        this.target = target;
+    }
+
+    @Override
+    public User save(User user) {
+        TransactionStatus txStatus = getTransactionStatus(false);
+        log.info("txStatus:[{}]", txStatus.getTransaction());
+        try {
+            User newUser = target.save(user);
+            txManager.commit(txStatus);
+            return newUser;
+        } catch (Exception exception) {
+            txManager.rollback(txStatus);
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public User findById(Long userId) {
+        TransactionStatus txStatus = getTransactionStatus(true);
+        log.info("txStatus:[{}]", txStatus.getTransaction());
+        try {
+            return target.findById(userId);
+        } catch (Exception exception) {
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public void delete(LoginUser loginUser) {
+        TransactionStatus txStatus = getTransactionStatus(false);
+        log.info("txStatus:[{}]", txStatus.getTransaction());
+        try {
+            target.delete(loginUser);
+            txManager.commit(txStatus);
+        } catch (Exception exception) {
+            txManager.rollback(txStatus);
+            throw new RuntimeException();
+        }
+    }
+
+    private TransactionStatus getTransactionStatus(boolean readOnly) {
+        try {
+            return txManager.getTransaction(createTransactionDefinition(readOnly));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+}
