25f932e5870779b04e462681c095f0723ed2cc75
devjun10 <devjun1023@gmail.com>
2024-02-13 01:50:07 +0900

    refactor: ThreadLocal 생성 로직 변경 - static끼리 어떤 객체가 먼저 생길지 순서가 정해지지 않아 이슈 발생.

--- jdbc/src/main/java/project/server/jdbc/core/transaction/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/project/server/jdbc/core/transaction/TransactionSynchronizationManager.java
@@ -8,24 +8,26 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public final class TransactionSynchronizationManager {
 
+    private static final ThreadLocal<Map<DataSource, Object>> factory = new ThreadLocal<>();
+
     private TransactionSynchronizationManager() {
         throw new AssertionError("올바른 방식으로 생성자를 호출해주세요.");
-    }
-
-    private static final ThreadLocal<Map<DataSource, Object>> factory;
-
-    static {
-        factory = new ThreadLocal<>();
-        factory.set(new HashMap<>());
     }
 
     public static void bindResource(
         DataSource key,
         Object value
     ) {
-        Map<DataSource, Object> transactionMap = factory.get();
+        Map<DataSource, Object> transactionMap = getTransactionMap();
         transactionMap.put(key, value);
         log.debug("Put {} into threadlocal.", key);
+    }
+
+    private static Map<DataSource, Object> getTransactionMap() {
+        if (factory.get() == null) {
+            factory.set(new HashMap<>());
+        }
+        return factory.get();
     }
 
     public static void releaseResource(DataSource key) {
