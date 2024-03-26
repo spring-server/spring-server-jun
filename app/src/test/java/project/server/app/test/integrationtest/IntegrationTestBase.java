49547fac6c1950c665c77e5be25f0c5234cd8e80
devjun10 <devjun1023@gmail.com>
2024-03-26 16:15:42 +0900

    test: @AfterEach -> @BeforeEach로 변경 - 테스트 전 모든 데이터를 제거하도록 수정

--- app/src/test/java/project/server/app/test/integrationtest/IntegrationTestBase.java
+++ app/src/test/java/project/server/app/test/integrationtest/IntegrationTestBase.java
@@ -1,6 +1,7 @@
 package project.server.app.test.integrationtest;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import project.server.app.core.domain.user.UserRepository;
 import project.server.app.core.web.user.persistence.UserPersistenceRepository;
 import project.server.mvc.springframework.context.ApplicationContext;
@@ -21,7 +22,7 @@ public abstract class IntegrationTestBase {
         }
     }
 
-    @AfterEach
+    @BeforeEach
     void setUp() {
         userRepository.clear();
     }
